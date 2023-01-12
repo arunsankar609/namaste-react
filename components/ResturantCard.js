c52f3051c95221539e17cc8ef7f641f80e206f9f
arunsankar609 <arunsankar248@gmail.com>
2023-01-12 17:19:45 +0530

    chapter 5 updated

---
+++ components/ResturantCard.js
@@ -0,0 +1,16 @@
+import { IMG_URL } from "../configs/config";
+const RestCard = (props) => {
+    return (
+      <div className="card">
+        <img
+          className="cardImg"
+          src={IMG_URL + props.restaurant.data?.cloudinaryImageId}
+          alt="cardImage"
+        />
+        <h2>{props.restaurant.data?.name}</h2>
+        <h3>{props.restaurant.data?.cuisines.join(",")}</h3>
+        <h4 className="rating">Rating {props.restaurant.data?.avgRating}</h4>
+      </div>
+    );
+  };
+   export default RestCard

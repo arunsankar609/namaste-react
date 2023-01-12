080a113ce9e120ea6b442ebf978030bcb15bd75d
arunsankar609 <arunsankar248@gmail.com>
2023-01-12 22:56:00 +0530

    chapter 5 completed

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ import ReactDOM from "react-dom/client";
 import Headert from "./components/Header";
 
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 const Appon1 = () => {
   return (
     <div id="container">
       <Headert />
       <Body />
+      <Footer/>
     </div>
   );
 };
